05eb356737f8cf63e62f1182bc7af46cb7e65766
zgbjgg <zgbjgg@gmail.com>
2025-03-28 01:53:17 -0600

    fix main module to handle docs on hex

--- lib/dragonite.ex
+++ lib/dragonite.ex
@@ -1,21 +1,5 @@
-defmodule Dragonite.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
-  use Application
-
-  require Logger
-
-  def start(_type, _args) do
-    children = [
-      # Start the Config rules server
-      {Dragonite.Config, []}
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Fr8hubEdi.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+defmodule Dragonite do
+  @moduledoc """
+  Dragonite - Fast, reliable and configurable EDI parser (encode & decode), seeker & rule runner.
+  """
 end
